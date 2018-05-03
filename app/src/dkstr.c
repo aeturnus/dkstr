1c323b92098df5cde3fa3360cdd9e1308407d2cf
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2018-05-03 00:08:46 +0000

    app: added profiling commands + fixed infinite loops

--- app/src/dkstr.c
+++ app/src/dkstr.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <math.h>
 #include <unistd.h>
 #include <time.h>
 
@@ -159,7 +160,14 @@ void hw_gen_path(int w, int h, const coord * start, const coord * end,
     uint8_t dir = 0;
     uint8_t prev_dir = 0xFF;
     coord curr = *end;
+    int count = 0;
+    int max = w * h;
     while (!(curr.x == start->x && curr.y == start->y)) {
+        if (count++ >= max) {
+            //printf("Max hit\n");
+            return;
+        }
+
         dir = hw_get(buffer, w, h, curr.x, curr.y);
         if (dir & 0x8)
             dir &= 0x7;
@@ -233,7 +241,7 @@ int hw_pathfind(const map * map, const coord * start, const coord * end, path * 
     while ((*dkstr) & CTRL_RUN)
         ++loops;
     prof_end(prof); prof->exec += prof_dt(prof);
-    printf("poll loops: %d\n", loops);
+    //printf("poll loops: %d\n", loops);
 
     ///*
     // transfer back
@@ -311,6 +319,154 @@ int play_map(const char * map_path, int hw, const coord * start, const coord * e
     return 0;
 }
 
+typedef struct data_point_
+{
+    uint64_t n;
+    uint64_t min;
+    uint64_t max;
+    double   avg;
+    double   sd;
+} data_point;
+
+static
+void calc_stats(data_point * p, uint64_t * data, int n)
+{
+    uint64_t max = 0;
+    uint64_t min = UINT64_MAX;
+    uint64_t sum = 0;
+    double avg = 0.0;
+    double var = 0.0;
+    double sd  = 0.0;
+    for (int i = 0; i < n; ++i) {
+        uint64_t s = data[i];
+
+        if (s > max)
+            max = s;
+        if (s < min)
+            min = s;
+
+        sum += s;
+    }
+    avg = ((double) sum) / ((double) n);
+
+    for (int i = 0; i < n; ++i) {
+        double s = (double) data[i];
+        double diff = s - avg;
+        var += diff * diff;
+    }
+    var /= (double) (n - 1);
+    sd = sqrt(var);
+
+    p->n   = n;
+    p->max = max;
+    p->min = min;
+    p->avg = avg;
+    p->sd  = sd;
+}
+
+void print_stats(data_point * p)
+{
+    printf("    Min (ns): %llu\n", p->min);
+    printf("    Max (ns): %llu\n", p->max);
+    printf("    Avg (ns): %0.2f\n", p->avg);
+    printf("    SD  (ns): %0.2f\n", p->sd);
+}
+
+int profile(unsigned int seed, int hw, int samples)
+{
+    // seed the things
+    map_seed(seed);
+    unsigned int coord_seed = ~seed;
+
+    uint64_t * prproc_samples = (uint64_t *) malloc(sizeof(uint64_t) * samples);
+    uint64_t * tx_samples = (uint64_t *) malloc(sizeof(uint64_t) * samples);
+    uint64_t * exec_samples = (uint64_t *) malloc(sizeof(uint64_t) * samples);
+    uint64_t * rx_samples = (uint64_t *) malloc(sizeof(uint64_t) * samples);
+    uint64_t * poproc_samples = (uint64_t *) malloc(sizeof(uint64_t) * samples);
+    uint64_t * total_samples = (uint64_t *) malloc(sizeof(uint64_t) * samples);
+
+    mem_context mem_bram, mem_dkstr;
+    uint32_t * bram_map, * bram_dir, * dkstr;
+    if (hw) {
+        if (mem_ctor(&mem_bram, MEM_MMAP, 1, (void*)(uintptr_t) 0x40000000, (void*)(uintptr_t) 0x40001fff) != MEM_OKAY)
+            return 1;
+        if (mem_ctor(&mem_dkstr, MEM_MMAP, 1, (void*)(uintptr_t) 0x40004000, (void*)(uintptr_t) 0x40004fff) != MEM_OKAY)
+            return 1;
+        bram_map = mem_addr(&mem_bram, (void*)(uintptr_t) 0x40000000);
+        bram_dir = mem_addr(&mem_bram, (void*)(uintptr_t) 0x40001000);
+        dkstr = mem_addr(&mem_dkstr, (void*)(uintptr_t) 0x40004000);
+    }
+
+    for (int i = 0; i < samples; ++i) {
+        map map;
+        path path;
+        coord start, end;
+        prof prof;
+        prof_ctor(&prof);
+
+        map_rand(&map, 28, 28);
+        start.x = rand_r(&coord_seed) % 28;
+        start.y = rand_r(&coord_seed) % 28;
+        end.x = rand_r(&coord_seed) % 28;
+        end.y = rand_r(&coord_seed) % 28;
+
+        if (hw)
+            hw_pathfind(&map, &start, &end, &path, bram_map, bram_dir, dkstr, &prof);
+        else
+            path_find(&map, &start, &end, &path, &prof);
+
+        prproc_samples[i] = prof.prproc;
+        tx_samples[i] = prof.tx;
+        exec_samples[i] = prof.exec;
+        rx_samples[i] = prof.rx;
+        poproc_samples[i] = prof.poproc;
+        total_samples[i] = prof.prproc + prof.tx + prof.exec + prof.rx + prof.poproc;
+
+        map_dtor(&map);
+        path_dtor(&path);
+    }
+
+    data_point prproc;
+    data_point tx;
+    data_point exec;
+    data_point rx;
+    data_point poproc;
+    data_point total;
+
+    calc_stats(&prproc, prproc_samples, samples);
+    calc_stats(&tx, tx_samples, samples);
+    calc_stats(&exec, exec_samples, samples);
+    calc_stats(&rx, rx_samples, samples);
+    calc_stats(&poproc, poproc_samples, samples);
+    calc_stats(&total, total_samples, samples);
+
+    printf("Samples taken: %d\n", samples);
+    printf("Pre-processing:\n");
+    print_stats(&prproc);
+    printf("Transfer to:\n");
+    print_stats(&tx);
+    printf("Execution:\n");
+    print_stats(&exec);
+    printf("Transfer from:\n");
+    print_stats(&rx);
+    printf("Post-processing:\n");
+    print_stats(&poproc);
+    printf("Total:\n");
+    print_stats(&total);
+
+    if (hw) {
+        mem_dtor(&mem_bram);
+        mem_dtor(&mem_dkstr);
+    }
+    free(prproc_samples);
+    free(tx_samples);
+    free(exec_samples);
+    free(rx_samples);
+    free(poproc_samples);
+    free(total_samples);
+    return 0;
+}
+
 int main(int argc, char * argv[])
 {
     if (argc < 2) {
@@ -349,7 +505,8 @@ int main(int argc, char * argv[])
             hw = 1;
 
         return play_map(argv[2], hw, &start, &end);
-    } else if (!strcmp("rand", argv[1])) {
+    }
+    else if (!strcmp("rand", argv[1])) {
         if (argc <= 6) {
             fprintf(stderr, "ERROR: dkstr rand <seed> <start_x> <start_y> <end_x> <end_y> [sw,hw; default sw]\n");
         }
@@ -369,6 +526,24 @@ int main(int argc, char * argv[])
 
         map_seed(seed);
         return play_map(NULL, hw, &start, &end);
+    }
+    else if (!strcmp("profile", argv[1])) {
+        if (argc < 4) {
+            fprintf(stderr, "ERROR: dkstr profile <sw, hw> <samples> [seed]\n");
+        }
+
+        unsigned int seed = time(NULL);
+        int hw = 0;
+        int samples;
+
+        if (!strcmp(argv[2], "hw"))
+            hw = 1;
+        sscanf(argv[3], "%d", &samples);
+        if (argc >= 5)
+            sscanf(argv[4], "%u", &seed);
+
+        return profile(seed, hw, samples);
+
     }
     else {
         fprintf(stderr, "ERROR: invalid command %s\n", argv[1]);
